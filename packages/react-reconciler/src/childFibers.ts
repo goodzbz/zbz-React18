b6fa0869cde0dd880c5c479f210e5680c4325a07
bozhenzhao <2661469338@qq.com>
2023-10-12 19:54:45 +0800

    feat: 实现多节点Diff

--- packages/react-reconciler/src/childFibers.ts
+++ packages/react-reconciler/src/childFibers.ts
@@ -10,6 +10,7 @@ import { REACT_ELEMENT_TYPE } from 'shared/ReactSymbols';
 import { HostText } from './workTags';
 import { ChildDeletion, Placement } from './fiberFlags';
 
+type ExistingChildren = Map<string | number, FiberNode>;
 function ChildReconciler(shouldTrackEffect: boolean) {
 	function deleteChild(returnFiber: FiberNode, childToDelete: FiberNode) {
 		if (!shouldTrackEffect) {
@@ -111,7 +112,104 @@ function ChildReconciler(shouldTrackEffect: boolean) {
 		returnFiber: FiberNode,
 		currentFirstChild: FiberNode | null,
 		newChild: any[]
-	) {}
+	) {
+		// 最后一个可复用的	fiber在current中的index
+		let lastPlacedIndex: number = 0;
+		//创建最后一个fiber
+		let lastNewFiber: FiberNode | null = null;
+		//创建第一个fiber
+		let fistNewFiber: FiberNode | null = null;
+
+		// 1  将current 保存在map中
+		const existingChildren: ExistingChildren = new Map();
+		let current = currentFirstChild;
+		while (current !== null) {
+			const keyToUse = current.key !== null ? current.key : current.index;
+			existingChildren.set(keyToUse, current);
+			current = current.sibling;
+		}
+		for (let i = 0; i < newChild.length; i++) {
+			// 2 遍历newChild 寻找是否可复用
+			const after = newChild[i];
+			const newFiber = updateFormMap(returnFiber, existingChildren, i, after);
+
+			if (newFiber === null) {
+				continue;
+			}
+			// 3 标记移动还是插入
+			newFiber.index = i;
+			newFiber.return = returnFiber;
+			if (lastNewFiber === null) {
+				lastNewFiber = newFiber;
+				fistNewFiber = newFiber;
+			} else {
+				lastNewFiber.sibling = newFiber;
+				lastNewFiber = lastNewFiber.sibling;
+			}
+			if (!shouldTrackEffect) {
+				continue;
+			}
+			const current = newFiber.alternate;
+			if (current !== null) {
+				const oldIndex = current.index;
+				if (oldIndex < lastPlacedIndex) {
+					//移动
+					newFiber.flags |= Placement;
+					continue;
+				} else {
+					// 不移动
+					lastPlacedIndex = oldIndex;
+				}
+			} else {
+				// mount
+				newFiber.flags |= Placement;
+			}
+		}
+		// 4. 将Map中剩下的标记为删除
+		existingChildren.forEach((fiber) => {
+			deleteChild(returnFiber, fiber);
+		});
+		return fistNewFiber;
+	}
+
+	function updateFormMap(
+		returnFiber: FiberNode,
+		existingChildren: ExistingChildren,
+		index: number,
+		element: any
+	) {
+		const keyToUse = element.key !== null ? element.key : element.index;
+		const before = existingChildren.get(keyToUse);
+		if (typeof element === 'string' || typeof element === 'number') {
+			// HostText
+			if (before) {
+				if (before.tag === HostText) {
+					existingChildren.delete(keyToUse);
+					return useFiber(before, { content: element + '' });
+				}
+			}
+			return new FiberNode(HostText, { content: element + '' }, null);
+		}
+		if (typeof element === 'object' && element !== null) {
+			switch (element.$$typeof) {
+				case REACT_ELEMENT_TYPE:
+					if (before) {
+						if (before.type === element.type) {
+							existingChildren.delete(keyToUse);
+							return useFiber(before, element.props);
+						}
+					}
+
+					return createFiberFromElement(element);
+			}
+			// TODO 数组类型
+			if (Array.isArray(element) && __DEV__) {
+				console.warn('还未实现数组类型的child');
+			}
+		}
+		return null;
+	}
+
 	return function reconcilerChildFibers(
 		returnFiber: FiberNode,
 		currentFiber: FiberNode | null,
@@ -131,6 +229,9 @@ function ChildReconciler(shouldTrackEffect: boolean) {
 		}
 
 		// TODO 多节点的情况 ul> li*3
+		if (Array.isArray(newChild)) {
+			return reconcileChildrenArray(returnFiber, currentFiber, newChild);
+		}
 
 		// HostText
 		if (typeof newChild === 'string' || typeof newChild === 'number') {
