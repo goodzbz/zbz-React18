2db1f3fecde4e16ee81bba438393aba4495a0c54
bozhenzhao <2661469338@qq.com>
2023-10-20 16:08:54 +0800

    chore: 实现并发更新Demo

---
+++ demos/test-fc/main.ts
@@ -0,0 +1,121 @@
+import './style.css';
+import {
+	unstable_ImmediatePriority as ImmediatePriority,
+	unstable_UserBlockingPriority as UserBlockingPriority,
+	unstable_NormalPriority as NormalPriority,
+	unstable_LowPriority as LowPriority,
+	unstable_IdlePriority as IdlePriority,
+	unstable_scheduleCallback as scheduleCallback,
+	unstable_shouldYield as shouldYield,
+	CallbackNode,
+	unstable_getFirstCallbackNode as getFirstCallbackNode,
+	unstable_cancelCallback as cancelCallback,
+} from 'scheduler';
+
+const root = document.querySelector('.root');
+console.log(root);
+
+type Priority =
+	| typeof IdlePriority
+	| typeof LowPriority
+	| typeof NormalPriority
+	| typeof UserBlockingPriority
+	| typeof ImmediatePriority;
+
+interface Work {
+	count: number;
+	priority: Priority;
+}
+
+const workList: Work[] = [];
+let prevPriority: Priority = IdlePriority;
+let curCallback: CallbackNode | null = null;
+
+[LowPriority, NormalPriority, UserBlockingPriority, ImmediatePriority].forEach(
+	(priority) => {
+		const btn = document.createElement('button');
+		console.log(btn);
+		root?.appendChild(btn);
+		console.log(priority);
+		btn.innerText = [
+			'',
+			'ImmediatePriority',
+			'UserBlockingPriority',
+			'NormalPriority',
+			'LowPriority',
+		][priority];
+		btn.onclick = () => {
+			workList.unshift({
+				count: 100,
+				priority: priority as Priority,
+			});
+			schedule();
+		};
+	}
+);
+
+function schedule() {
+	const cbNode = getFirstCallbackNode();
+	const curWork = workList.sort((w1, w2) => w1.priority - w2.priority)[0];
+	// TODO 策略逻辑
+
+	if (!curWork) {
+		curCallback = null;
+		cbNode && cancelCallback(cbNode);
+		return;
+	}
+	const { priority: curPriorty } = curWork;
+
+	if (curPriorty === prevPriority) {
+		return;
+	}
+	// 更高优先级的work
+	cbNode && cancelCallback(cbNode);
+	scheduleCallback(curPriorty, perform.bind(null, curWork));
+}
+/**
+ *   1. work.priority
+ * 	 2. 饥饿问题
+ *	 3. 时间贴片
+ */
+function perform(work: Work, didTimeout?: boolean) {
+	const needSync = work.priority === ImmediatePriority || didTimeout;
+	while ((needSync || !shouldYield()) && work.count) {
+		work.count--;
+		insertSpan(work.priority + '');
+	}
+	// 中断执行|| 执行完
+	prevPriority = work.priority;
+
+	if (!work.count) {
+		console.log('this is finish');
+		const workIndex = workList.indexOf(work);
+		workList.splice(workIndex, 1);
+		console.log(workList);
+		prevPriority = 4;
+	}
+	const prevCallback = curCallback;
+	console.log(prevCallback);
+	schedule();
+	const newCallback = curCallback;
+	if (newCallback && prevCallback === newCallback) {
+		return perform.bind(null, work);
+	}
+
+	return perform.bind(null, work);
+}
+
+function insertSpan(content) {
+	const span = document.createElement('span');
+	span.innerHTML = content;
+	span.className = `pri-${content}`;
+	doSomeBuzyWork(100000);
+	root?.appendChild(span);
+}
+
+function doSomeBuzyWork(len: number) {
+	let result = 0;
+	while (len--) {
+		result += len;
+	}
+}
